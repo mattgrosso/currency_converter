d1eedf086bfd544e25a5c62a5e68e2d2a4227193
mattgrosso <mattgrosso@gmail.com>
2017-02-20 10:54:15 -0500

    Seperated class definitions from main app.

--- currency.rb
+++ currency.rb
@@ -1,3 +1,44 @@
-test = "this is working."
+class Currency
 
-p test
+  def initialize(currency_code, ammount = 0)
+    @currency_code = currency_code
+    @ammount = ammount
+  end
+
+  def currency_code
+    @currency_code
+  end
+
+  def ammount
+    @ammount
+  end
+
+  def == (other)
+    if other.currency_code == @currency_code && other.ammount == @ammount
+      return true
+    end
+    return false
+  end
+
+  def + (other)
+    if other.currency_code == @currency_code
+      total = other.ammount + @ammount
+      return Currency.new(@currency_code, total)
+    end
+    raise Exception.new("DifferentCurrencyCodeError")
+  end
+
+  def - (other)
+    if other.currency_code == @currency_code
+      total = other.ammount - @ammount
+      return Currency.new(@currency_code, total)
+    end
+    raise Exception.new("DifferentCurrencyCodeError")
+  end
+
+  def * (other)
+    product = other * @ammount
+    Currency.new(@currency_code, product)
+  end
+
+end
