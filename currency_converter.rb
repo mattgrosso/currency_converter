9dad547a568a2df1727ab90c96cc1e15e6e88ee2
mattgrosso <mattgrosso@gmail.com>
2017-02-21 09:32:26 -0500

    Added exchange_rates method to currency_converter.

--- currency_converter.rb
+++ currency_converter.rb
@@ -9,25 +9,29 @@ class Currency_converter
     }
   end
 
+  def exchange_rates
+    @exchange_rates
+  end
+
   def convert(currency, currency_code = nil)
 
-    if !@exchange_rates[currency_code] && currency_code != nil
+    if !exchange_rates[currency_code] && currency_code != nil
       raise Exception.new("UnknownCurrencyCodeError")
     end
 
     if currency_code
-      rate = @exchange_rates[currency_code]/@exchange_rates[currency.currency_code]
+      rate = exchange_rates[currency_code]/exchange_rates[currency.currency_code]
       return Currency.new(currency_code, currency.ammount * rate)
     end
 
     puts "You've given me #{currency.ammount}#{currency.currency_code}. What would you like to change that into?"
     input = gets.chomp
 
-    if !@exchange_rates[input]
+    if !exchange_rates[input]
       raise Exception.new("UnknownCurrencyCodeError")
     end
 
-    rate = @exchange_rates[input]/@exchange_rates[currency.currency_code]
+    rate = exchange_rates[input]/exchange_rates[currency.currency_code]
     return Currency.new(input, currency.ammount * rate)
   end
 end
